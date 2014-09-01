7ff8b1824d603208eb22e1f5f09d28ef5268ab9e
Denk Denys <denkenrage@gmail.com>
2014-09-01 21:45:57 +0300

    2

---
+++ app/datatables/avrs_datatable.rb
@@ -0,0 +1,63 @@
+class AvrsDatatable
+  delegate :params, :h, :link_to, :number_to_currency, to: :@view
+
+  def initialize(view)
+    @view = view
+  end
+
+  def as_json(options = {})
+    {
+        sEcho: params[:sEcho].to_i,
+        iTotalRecords: Avr.count,
+        iTotalDisplayRecords: avrs.total_entries,
+        aaData: data
+    }
+  end
+
+  private
+
+  def data
+    avrs.map do |avr|
+      [
+          link_to(avr.mdu, avr), #показать все аварии по данной мдю
+          link_to(avr.adress, avr), #показать данное мдю и адрес http://127.0.0.1:3000/mmms/№№№
+          link_to(avr.user, avr), # показать все аварии єтого юзверя
+          link_to(avr.type_avr, avr),
+          ERB::Util.h(avr.material),
+          ERB::Util.h(avr.comment),
+          ERB::Util.h(avr.date_on),
+          ERB::Util.h(avr.date_off)
+      ]
+    end
+  end
+
+  def avrs
+    @avrs ||= fetch_avrs
+  end
+
+  def fetch_avrs
+    avrs = Avr.order("#{sort_column} #{sort_direction}")
+    avrs = avrs.page(page).per_page(per_page)
+    if params[:sSearch].present?
+      avrs = avrs.where("mdu like :search or adress like :search", search: "%#{params[:sSearch]}%")
+    end
+    avrs
+  end
+
+  def page
+    params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 20
+  end
+
+  def sort_column
+    columns = %w[ mdu released_on adress]
+    columns[params[:iSortCol_0].to_i]
+  end
+
+  def sort_direction
+    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+  end
+end
